41e40002c48aba04e35d4bba5556c0a06a39653a
Danny Schreiber <danny@ravenartmedia.com>
2014-03-02 00:18:51 -0800

    added structuremap config and fixed repository

--- ModestoPower.Mvc/App_Start/BundleConfig.cs
+++ ModestoPower.Mvc/App_Start/BundleConfig.cs
@@ -20,9 +20,42 @@ namespace ModestoPower.Mvc
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-route.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/mp").Include(
+                      "~/Scripts/jquery.easing-1.3.pack.js",
+                      "~/Scripts/jquery.sticky.js",
+                      "~/Scripts/jquery.inview.js",
+                      "~/Scripts/jquery.flexslider-min.js",
+                      "~/Scripts/jquery.carouFredSel-6.2.1-packed.js",
+                      "~/Scripts/parallax.js",
+                      "~/Scripts/isotope.js",
+                      "~/Scripts/nlform.js",
+                      "~/Scripts/tweetable.jquery.min.js",
+                      "~/Scripts/jflickrfeed.min.js",
+                      "~/Scripts/odometer.min.js",
+                      "~/Scripts/jquery.waitforimages.js",
+                      "~/Scripts/jquery.knob.js",
+                      "~/Scripts/mediaelement-and-player.min.js",
+                      "~/Scripts/jquery.prettyPhoto.js",
+                      "~/Scripts/main.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/jasny-bootstrap.min.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/flexslider.css",
+                      "~/Content/animate.css",
+                      "~/Content/nlform.css",
+                      "~/Content/jquery.vegas.css",
+                      "~/Content/navigation.css",
+                      "~/Content/odometer-theme-default.css",
+                      "~/Content/magnific-popup.css",
+                      "~/Content/prettyPhoto.css",
+                      "~/Content/main.css",
+                      "~/Content/skins/color4.css"));
         }
     }
 }
