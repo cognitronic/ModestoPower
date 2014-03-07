fa4ffaf98223fab2638cc2ca606dcfa23f525c51
Danny Schreiber <danny@ravenartmedia.com>
2014-03-07 10:06:10 -0800

    created new data layer project for mongodb repositories

---
+++ RAM.Services/Cache/MongoDBCacheAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Web.Providers;
+
+namespace RAM.Services.Cache
+{
+    public class MongoDBCacheAdapter : ICacheStorage
+    {
+        private readonly static MongoDBOutputCacheProvider _instance;
+
+        static MongoDBCacheAdapter()
+        {
+            _instance = new MongoDBOutputCacheProvider();
+        }
+
+        public static MongoDBOutputCacheProvider Instance { get { return _instance; } }
+        public void Remove(string key)
+        {
+            Instance.Remove(key);
+        }
+
+        public void Store(string key, object data)
+        {
+            Instance.Add(key, data, DateTime.Now.AddDays(7));
+        }
+
+        public T Get<T>(string key)
+        {
+            T itemStored = (T)Instance.Get(key);
+            if (itemStored == null)
+                itemStored = default(T);
+            return itemStored;
+        }
+    }
+}
