61cd993384aed6a47bc8c51a8efbde7c86df7f9a
Danny Schreiber <danny@ravenartmedia.com>
2014-03-08 08:35:59 -0800

    added routehandler that handles hyphenated routes and created views for about controller

--- ModestoPower.Mvc/App_Start/RouteConfig.cs
+++ ModestoPower.Mvc/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using RAM.Web.Routing;
 
 namespace ModestoPower.Mvc
 {
@@ -13,11 +14,12 @@ namespace ModestoPower.Mvc
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.Add(
+            new Route("{controller}/{action}/{id}",
+                new RouteValueDictionary(
+                    new { controller = "Home", action = "Index", id = UrlParameter.Optional }),
+                    new ModestoPower.Mvc.App_Start.HyphenatedRouteHandler())
+        );
         }
     }
 }
