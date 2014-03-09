33752a8e5e098c81c40716cda2c30afbf0e4d957
Danny Schreiber <danny@ravenartmedia.com>
2014-03-08 21:18:21 -0800

    creating landing views for all pages as well as their controllers

---
+++ RAM.Controllers/Controllers/ProgramsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RAM.Infrastructure.Authentication;
+using RAM.Services.Interfaces;
+using RAM.Controllers.ActionArguments;
+using RAM.Controllers.ViewModels;
+using System.Web.Mvc;
+
+namespace RAM.Controllers.Controllers
+{
+    public class ProgramsController : BaseController
+    {
+        private readonly IBlogService _blogService;
+        public ProgramsController(ILocalAuthenticationService authenticationService,
+            IUserService userService,
+            IBlogService blogService,
+            IExternalAuthenticationService externalAuthenticationService,
+            IFormsAuthentication formsAuthentication,
+            IActionArguments actionArguments)
+            : base(authenticationService, userService, externalAuthenticationService, actionArguments)
+        {
+
+            _blogService = blogService;
+        }
+
+
+        public ActionResult Index()
+        {
+            var view = new HomeView();
+            return View(view);
+
+        }
+    }
+}
